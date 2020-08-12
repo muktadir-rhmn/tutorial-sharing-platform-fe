eebddbcfa306328b46bb8625036e7e4466a94055
muktadirrahman <muktadir.rhmn@gmail.com>
2020-08-12 23:08:09 +0600

    create tutorial written

--- src/view/admin/tutorial/create/CreateTutorial.js
+++ src/view/admin/tutorial/create/CreateTutorial.js
@@ -3,30 +3,46 @@ import TextBox from "../../../form/TextBox";
 import TextArea from "../../../form/TextArea";
 import requester from "../../../../library/requester";
 import Button from "../../../form/Button";
+import formDataCollector from "../../../../library/formDataCollector";
 
 class CreateTutorial extends React.Component{
     constructor(props)  {
         super(props);
 
         this.state = {
-            rootCategory: [],
+            rootCategory: null,
         };
     }
 
     render() {
         console.log(this.state.rootCategory);
 
+        const options = [];
+        if (this.state.rootCategory != null) this.renderCategoryOptions(options, this.state.rootCategory.subcategories);
+        const categoryCombobox = <select id="categoryID" className="form-control">{options}</select>;
         return (
             <div>
                 <h1>Create Tutorial</h1>
-                <div>
-                    <TextBox label="Name"/>
-                    <TextArea label="Description" />
-
+                <div id="create-tutorial-form">
+                    <TextBox id="name" label="Name"/>
+                    <TextArea id="description" label="Description" />
+                    {categoryCombobox}
                     <Button label="Create" onClick={(event) => this.createTutorial(event)}/>
                 </div>
             </div>
         );
+    }
+
+    renderCategoryOptions(options, categories, indentation="") {
+        for (let i = 0; i < categories.length; i++) {
+            const option = (
+                <option key={categories[i].id} value={categories[i].id}>{`${indentation}${categories[i].name}`}</option>
+            )
+
+            options.push(option);
+
+            this.renderCategoryOptions(options, categories[i].subcategories, indentation + "---");
+        }
     }
 
     componentDidMount() {
@@ -45,7 +61,14 @@ class CreateTutorial extends React.Component{
     }
 
     createTutorial(event) {
+        const data = formDataCollector.collect("create-tutorial-form");
 
+        const path = `/tutorials`;
+        requester.POST(path, data).then(
+            (response) => {
+                alert("Tutorial added with ID " + response.tutorialID);
+            }
+        )
     }
 }
 
